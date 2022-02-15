77f4d34c240d7519cf77273fb6a4fa0057034943
Nickolas_Phen <nickolas.phen@gmail.com>
2022-02-15 03:23:18 -0500

    Database access

---
+++ pages/cups.tsx
@@ -0,0 +1,36 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import { useRouter } from "next/router"
+import {db} from "../firebase/clientApp"
+import {collection,getDocs} from "firebase/firestore";
+import {useState,useEffect} from "react";
+
+
+const Cups: NextPage = () => {
+    const [cups,setCups]=useState([]);
+    const cupsRef=collection(db,"cups");
+    useEffect(()=>{
+        const getCups=async()=>{
+            const data=await getDocs(cupsRef);
+            //setCups(data.docs.map((doc)=>({doc.data()})));
+            console.log(data)
+        };
+
+        getCups();
+    },[]);
+    
+
+
+    const router = useRouter()
+    const {
+      query: { id },
+    } = router
+
+    return(
+        <div>Viewing Cups</div>
+    )
+
+}
+
+export default Cups
