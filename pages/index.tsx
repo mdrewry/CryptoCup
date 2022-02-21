883a443a73294eac43179d38a9afdabe97564ded
Roho000 <rohan.samanta@hotmail.com>
2022-02-21 12:27:52 -0500

    made Footer in component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 import Logo from "../Icons/Logo.js";
@@ -14,6 +13,7 @@ import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
 import LocalAtmRoundedIcon from "@mui/icons-material/LocalAtmRounded";
 import FaqDiamond from "../Icons/FaqDiamond.js";
 import Grid from '@mui/material/Grid';
+import Footer from "../Components/Footer";
 
 const Home: NextPage = () => {
   return (
@@ -134,44 +134,8 @@ const Home: NextPage = () => {
         </p>
       </div>
 
-      <Grid className={styles.footer} container spacing={0}>
-        <Grid item xs={8.9}>
-          <div className={styles.containerLogo}>
-            <div className={styles.logo1}>
-              <Logo />
-            </div>
-            <h2 className={styles.title2}>Crypto Cup</h2>
-          </div>
-        </Grid>
-
-        <Grid item xs>
-          <div className={styles.footer1}>
-            <Grid item xs={3.5}>
-              <h6>ABOUT</h6>
-            </Grid>
-            <Grid item xs={3}>
-              <h6>FAQ</h6>
-            </Grid>
-            <Grid item xs>
-              <Link
-                href={{
-                  pathname: "/login"
-                }}
-                >
-                <a>LOG IN/SIGN UP</a>
-              </Link>
-            </Grid>
-          </div>
-          <Grid item xs className={styles.tos}>
-            <Grid item xs>
-              <h6>Insert License Here</h6>
-            </Grid>
-            <Grid item xs>
-              <h6>Terms of Service</h6>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Grid>
+      <Footer/>
+      
     </div>
   );
 };
