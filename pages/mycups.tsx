d141d402e532b47198d83868120a9a68a9ae070e
Nickolas_Phen <nickolas.phen@gmail.com>
2022-03-02 01:21:10 -0500

    Styling page

--- pages/mycups.tsx
+++ pages/mycups.tsx
@@ -1,12 +1,13 @@
 import type { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
+import cupstyles from '../styles/Cups.module.css'
 import { useRouter } from "next/router"
 import {db} from "../config/firebase.config"
 import {collection,getDocs,QueryDocumentSnapshot,DocumentData, Timestamp,addDoc} from "firebase/firestore";
 import {useState,useEffect} from "react";
 import {Cup} from "../lib.d";
-import { List,ListItem,ListItemText,ListSubheader } from '@mui/material'
-
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import moment from  'moment';
 
 const Cups: NextPage = () => {
     const [cups,setCups]=useState<QueryDocumentSnapshot<DocumentData>[]>([]);
@@ -41,7 +42,7 @@ const Cups: NextPage = () => {
       query: { id },
     } = router
 
-    const createCup=()=>{
+    /*const createCup=()=>{
         addDoc(cupsRef,{
             buyIn: 50,
             cryptosAvailable: ["a","b"],
@@ -54,23 +55,32 @@ const Cups: NextPage = () => {
             startDate: Date.now(),
             user: "String"
         });
-    }
+    }*/
     
+    const Img = styled('img')({
+        margin: 'auto',
+        display: 'block',
+        maxWidth: '316px',
+        maxHeight: '316px',
+      });
+
+      
     return(
-        <div className={styles.center}>
-            Viewing Cups
-            
+        <div className={cupstyles.padding}>
+            <h2>My Cups</h2>
+            <h4>Current Cups:</h4>
             {loading ? (<div>loading</div>) :
-                (cups.map((c)=>
-                <List
-                sx={{
-                    alignItems: 'center'
-                  }}>
-                    {c.get("director")}
-                </List>
-                ))
+            <Grid container >
+                {cups.map((c)=>
+                    <Grid item xs={12} md={6} lg={4} xl={3}>
+                        <h5>{c.get("name")}</h5>
+                        <div className={cupstyles.cuptype}>{c.get("cupType")}</div>
+                        <p>{moment(c.get("startDate")).format("M/D/YYYY")}-{moment(c.get("endDate")).format("M/D/YYYY")}</p>
+                    </Grid>
+                    )}
+            </Grid>  
             }
-            <button onClick={createCup}>Click Me to Create New Cup</button>
+            <h4>Upcoming Cups:</h4>
         </div>
         )
     
