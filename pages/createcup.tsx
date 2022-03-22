7b6c40aaf52895bc0ce3eed9c08807dbf9f35baf
mdrewry <mdrewry0@gmail.com>
2022-03-21 21:33:21 -0400

    -finished making form for create-cup

--- pages/createcup.tsx
+++ pages/createcup.tsx
@@ -1,25 +1,53 @@
 import React, { useState, useEffect } from "react";
 import type { NextPage } from "next";
 import createCupStyles from "../styles/createcup.module.css";
-import styles from "../styles/Login.module.css";
 import { makeStyles } from "@material-ui/core/styles";
-import Container from "@mui/material/Container";
 import Stack from "@mui/material/Stack";
 import IconButton from "@mui/material/IconButton";
+import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
 import CircleIcon from "@mui/icons-material/Circle";
+import AdapterMoment from "@mui/lab/AdapterMoment";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import DatePicker from "@mui/lab/DatePicker";
+import moment from "moment";
 const initValues = {
   cupName: "",
   password: "",
-  startDate: new Date(),
-  endDate: new Date(),
+  startDate: moment(new Date()).add(1, "days"),
+  endDate: moment(new Date()).add(2, "days"),
   buyIn: 0,
   inGameBudget: 1000,
 };
+
 const useStyles = makeStyles((theme) => ({
+  datePickerTextField: {
+    [`& fieldset`]: {
+      borderRadius: 25,
+    },
+    "&": {
+      marginTop: "9px",
+    },
+    "& .MuiFormHelperText-root": {
+      color: "#ffffff",
+    },
+    "& .MuiInputBase-root": {
+      width: "530px",
+      backgroundColor: "rgba(47, 56, 105, 0.6)",
+      borderRadius: 25,
+      color: "#ffffff",
+    },
+    "& .MuiButtonBase-root": {
+      color: "#ffffff",
+    },
+    "& .MuiInputBase-input": {
+      fontFamily: "Space Mono",
+      fontSize: 20,
+      padding: "15px 15px",
+    },
+  },
   textField: {
     [`& fieldset`]: {
       borderRadius: 25,
@@ -80,15 +108,15 @@ const CreateCup: NextPage = () => {
           : "Password must be at least 6 characters long containing numbers and letters.";
     }
     if ("startDate" in fieldValues) {
-      if (fieldValues.startDate < new Date())
+      if (fieldValues.startDate < moment(new Date()))
         temp.startDate = "Start date must be after current date.";
       else if (fieldValues.startDate >= values.endDate)
-        temp.endDate = "Start date must be before end date.";
-      else temp.endDate = "";
+        temp.startDate = "Start date must be before end date.";
+      else temp.startDate = "";
     }
     if ("endDate" in fieldValues) {
-      if (fieldValues.endDate < new Date())
-        temp.endDate = "Start date must be after current date.";
+      if (fieldValues.endDate < moment(new Date()))
+        temp.endDate = "End date must be after current date.";
       else if (fieldValues.endDate <= values.startDate)
         temp.endDate = "End date must be after start date.";
       else temp.endDate = "";
@@ -175,31 +203,49 @@ const CreateCup: NextPage = () => {
                 </Grid>
                 <Grid item xs={12} md={6}>
                   <p className={createCupStyles.fieldName}>Start Date: </p>
-                  <TextField
-                    className={classes.textField}
-                    name="buyIn"
-                    type="number"
-                    onChange={handleInputValue}
-                    onBlur={handleInputValue}
-                    {...(errors["buyIn"] && {
-                      error: true,
-                      helperText: errors["buyIn"],
-                    })}
-                  />
+                  <LocalizationProvider dateAdapter={AdapterMoment}>
+                    <DatePicker
+                      value={values.startDate}
+                      onChange={(newValue) =>
+                        handleInputValue({
+                          target: { name: "startDate", value: newValue },
+                        })
+                      }
+                      renderInput={(params) => (
+                        <TextField
+                          className={classes.datePickerTextField}
+                          {...(errors["startDate"] && {
+                            error: true,
+                            helperText: errors["startDate"],
+                          })}
+                          {...params}
+                        />
+                      )}
+                    />
+                  </LocalizationProvider>
                 </Grid>
                 <Grid item xs={12} md={6}>
                   <p className={createCupStyles.fieldName}>End Date: </p>
-                  <TextField
-                    className={classes.textField}
-                    name="buyIn"
-                    type="number"
-                    onChange={handleInputValue}
-                    onBlur={handleInputValue}
-                    {...(errors["buyIn"] && {
-                      error: true,
-                      helperText: errors["buyIn"],
-                    })}
-                  />
+                  <LocalizationProvider dateAdapter={AdapterMoment}>
+                    <DatePicker
+                      value={values.endDate}
+                      onChange={(newValue) =>
+                        handleInputValue({
+                          target: { name: "endDate", value: newValue },
+                        })
+                      }
+                      renderInput={(params) => (
+                        <TextField
+                          className={classes.datePickerTextField}
+                          {...(errors["endDate"] && {
+                            error: true,
+                            helperText: errors["endDate"],
+                          })}
+                          {...params}
+                        />
+                      )}
+                    />
+                  </LocalizationProvider>
                 </Grid>
                 <Grid item xs={12}>
                   <p className={createCupStyles.fieldName}>Buy-In: </p>
@@ -217,7 +263,7 @@ const CreateCup: NextPage = () => {
                 </Grid>
                 <Grid item xs={12}>
                   <p className={createCupStyles.fieldName}>
-                    Player In-Game Budget:{" "}
+                    Player In-Game Budget:
                   </p>
                   <TextField
                     className={classes.textField}
@@ -230,6 +276,26 @@ const CreateCup: NextPage = () => {
                       helperText: errors["inGameBudget"],
                     })}
                   />
+                </Grid>
+                <Grid className={createCupStyles.center} item xs={12}>
+                  <Button
+                    style={{
+                      marginTop: "20px",
+                      background: "rgba(47, 56, 105, 0.6)",
+                      fontFamily: "Space Mono",
+                      fontSize: 20,
+                      borderRadius: 25,
+                      fontWeight: 700,
+                      height: 50,
+                      padding: 10,
+                      width: 214,
+                      color: "white",
+                      textTransform: "none",
+                    }}
+                    type="submit"
+                  >
+                    Create Cup
+                  </Button>
                 </Grid>
               </Grid>
             )}
