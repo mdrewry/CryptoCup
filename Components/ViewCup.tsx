ba7fe42b71c6df134a794d3020d72e33ebeaf04d
mdrewry <mdrewry0@gmail.com>
2022-04-06 00:17:55 -0400

    -fixed loading issue with viewcup

--- Components/ViewCup.tsx
+++ Components/ViewCup.tsx
@@ -1,120 +1,118 @@
 import cupstyles from "../styles/Cups.module.css";
 import Router, { useRouter } from "next/router";
-
 import { db } from "../config/firebase.config";
 import {
   collection,
   getDoc,
-  getDocs,
-  DocumentSnapshot,
-  DocumentData,
   doc,
   where,
   query,
   onSnapshot,
-  DocumentReference,
-  Timestamp,
 } from "firebase/firestore";
 import { useState, useEffect, useContext } from "react";
+import CircularProgress from "@mui/material/CircularProgress";
 import Grid from "@mui/material/Grid";
 import moment from "moment";
 import { UserContext } from "../context/UserProvider";
 import Button from "@mui/material/Button";
-
 type ContentProps = { filter: Number; cupNameFilter: String };
 const Cups = ({ filter, cupNameFilter }: ContentProps) => {
-  const user = useContext(UserContext);
-  const [cups, setCups] = useState<DocumentSnapshot<DocumentData>[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-  const cupsRef = collection(db, "cups");
-
-  const getCups = async () => {
-    const userDocRef = doc(collection(db, "users"), user.uid);
-    const userQuery = query(
-      collection(db, "cupsInUser"),
-      where("userID", "==", userDocRef)
-    );
-    const result: DocumentSnapshot<DocumentData>[] = [];
-    //filter==0 for ongoing cups(player), filter==1 for upcoming cups(player based), filter==2 for ALL upcoming cups
-    if (filter == 2) {
-      const data = await getDocs(cupsRef);
-      data.forEach((c) => {
-        if (moment(c.get("startDate").toDate()) > moment()) {
-          result.push(c);
-        }
-        //result.push(c);
-      });
-    } else {
-      onSnapshot(userQuery, (snapshot) => {
-        const userCups: DocumentReference[] = snapshot.docs.at(0)?.data().cups;
-        console.log(userCups);
-        if (userCups != null) { 
-          userCups.forEach((c) => {
-            getDoc(c).then((d) => {
-              if (
-                filter == 0 &&
-                (moment(d.get("startDate").toDate()) <= moment()) &&
-                (moment(d.get("endDate").toDate()) > moment())
-              ) {
-                result.push(d);
-              } else if (filter == 1 && moment(d.get("startDate").toDate()) > moment()) {
-                result.push(d);
-              }
-            });
-          });
-        }
-      });
-    }
-    // setCups(data.docs.map((item)=>{
-    //     return {...item.data(),id:item.id}
-    // }));
-
-    setCups(result);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getCups();
-  }, []);
-
   const router = useRouter();
   const {
     query: { id },
   } = router;
 
-  // const createCup=()=>{
-  //     addDoc(cupsRef,{
-  //         buyIn: 50,
-  //         cryptosAvailable: ["a","b"],
-  //         cupType: "String",
-  //         currentState: 2,
-  //         director: "String",
-  //         endDate: Date.now(),
-  //         name: "String",
-  //         playerCuts: [1,2,3],
-  //         startDate: Date.now(),
-  //         user: "String"
-  //     });
-  // }
+  const user = useContext(UserContext);
+  const [cups, setCups] = useState<Array<any>>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const userDocRef = doc(collection(db, "users"), user.uid);
+    const cupsInUserRef = query(
+      collection(db, "cupsInUser"),
+      where("userID", "==", userDocRef)
+    );
+    //filter==0 for ongoing cups(player), filter==1 for upcoming cups(player based), filter==2 for ALL upcoming cups
+    if (filter == 2) {
+      const unsubscribeSnapshot = onSnapshot(
+        collection(db, "cups"),
+        (snapshot) => {
+          let cups: Array<any> = [];
+          snapshot.docs.forEach((doc: any) => {
+            const data: any = doc.data();
+
+            const startDate = data.startDate.toDate();
+            const endDate = data.endDate.toDate();
+            if (moment(startDate) > moment())
+              cups.push({
+                ...data,
+                startDate,
+                endDate,
+                id: doc.id,
+                ref: doc.ref,
+              });
+          });
+          setCups(cups);
+          setLoading(false);
+        }
+      );
+      return unsubscribeSnapshot;
+    }
+    const unsubscribeSnapshot = onSnapshot(cupsInUserRef, async (snapshot) => {
+      let results: Array<any> = [];
+      const userCups: Array<any> = snapshot.docs[0]?.data().cups;
+      if (userCups !== null) {
+        await Promise.all(
+          userCups.map(async (cupRef: any) => {
+            const doc = await getDoc(cupRef);
+            const data: any = doc.data();
+            const startDate = data.startDate.toDate();
+            const endDate = data.endDate.toDate();
+            if (
+              filter == 0 &&
+              moment(startDate) <= moment() &&
+              moment(endDate) > moment()
+            )
+              results.push({
+                ...data,
+                startDate,
+                endDate,
+                id: doc.id,
+                ref: doc.ref,
+              });
+            else if (filter == 1 && moment(startDate) > moment())
+              results.push({
+                ...data,
+                startDate,
+                endDate,
+                id: doc.id,
+                ref: doc.ref,
+              });
+            return 0;
+          })
+        );
+      }
+      setCups(results);
+      setLoading(false);
+    });
+    return unsubscribeSnapshot;
+  }, []);
   const handleRedirect = (route: string) => {
     Router.push(`cups/${route}`);
   };
   return (
     <div>
       {loading ? (
-        <p>loading</p>
+        <CircularProgress />
       ) : (
         <Grid container spacing={3}>
           {cups
             .filter((c) => {
               if (cupNameFilter === "") return true;
               else
-                return (
-                  c
-                    .get("name")
-                    .toLowerCase()
-                    .includes(cupNameFilter.toLowerCase())
-                );
+                return c.name
+                  .toLowerCase()
+                  .includes(cupNameFilter.toLowerCase());
             })
             .map((c) => (
               <Grid item xs={12} md={6} lg={4} xl={3}>
@@ -122,13 +120,14 @@ const Cups = ({ filter, cupNameFilter }: ContentProps) => {
                   <div style={{ textAlign: "left", textTransform: "none" }}>
                     <img
                       className={cupstyles.placeholder}
-                      src={c.get("imageURL")}
+                      src={c.imageURL}
                     ></img>
-                    <h5 className={cupstyles.name}>{c.get("name")}</h5>
-                    <div className={cupstyles.cuptype}>{c.get("cupType")}</div>
+                    <h5 className={cupstyles.name}>{c.name}</h5>
+                    <div className={cupstyles.cuptype}>{c.cupType}</div>
                     <p>
-                      {moment(c.get("startDate").toDate()).format("M/D/YYYY")}&nbsp;-&nbsp;
-                      {moment(c.get("endDate").toDate()).format("M/D/YYYY")}
+                      {moment(c.startDate).format("M/D/YYYY")}
+                      &nbsp;-&nbsp;
+                      {moment(c.endDate).format("M/D/YYYY")}
                     </p>
                   </div>
                 </Button>
