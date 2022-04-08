d563d049b5b87e5eaa9dc6380e60f77442571562
Amer Khalifa <68618099+amerkhalifa@users.noreply.github.com>
2022-04-08 10:36:13 -0400

    fixed current earnings

--- pages/cups/[cupid].tsx
+++ pages/cups/[cupid].tsx
@@ -15,6 +15,8 @@ import Leaderboard from "../../Components/Leaderboards";
 import CupWallet from "../../Components/CupWallet";
 import Grid from "@mui/material/Grid";
 import WalletLine from "../../Icons/WalletLine.js";
+import { CryptoContext } from "../../context/CryptoProvider";
+
 
 const CupDetails: NextPage = () => {
   const router = useRouter();
@@ -28,6 +30,7 @@ const CupDetails: NextPage = () => {
   const [cupState, setCupState] = useState("");
   const [buyIn, setBuyIn] = useState(0);
   const [totalBudget, setTotalBudget] = useState(0);
+  const [earnings, setEarnings] = useState(0);
   const [startDate, setStartDate] = useState(Timestamp.now());
   const [endDate, setEndDate] = useState(Timestamp.now());
   const [joinedUser, setJoinedUser] = useState(false);
@@ -37,6 +40,7 @@ const CupDetails: NextPage = () => {
     query: { id },
   } = router;
   const user = useContext(UserContext);
+  const cryptos = useContext(CryptoContext);
 
   useEffect(() => {
     const cupDocRef = doc(db, "cups", cupid);
@@ -54,8 +58,15 @@ const CupDetails: NextPage = () => {
         setEthAddress(data.ethAddress);
         setUserPortfolios(data.userPortfolios);
         setTotalBudget(data.totalBudget);
+        
         if (user.uid in data.userPortfolios) {
           setJoinedUser(true);
+          let total = 0;
+          Object.entries(data.userPortfolios[user.uid]).map((x: any) => {
+            total = total + cryptos[x[0]].price * x[1];
+          });
+          total = parseFloat(total.toFixed(2));
+          setEarnings(total);
         }
         const userDocRef = doc(db, "users", data.director);
         const userDocSnap = await getDoc(userDocRef);
@@ -135,7 +146,7 @@ const CupDetails: NextPage = () => {
                   <CupWallet cupid={cupid} portfolios={userPortfolios} />
                   <WalletLine />
                   <div className={styles.total}>
-                    <h6>Total: ${userPortfolios[user.uid]["USD"]} USD</h6>
+                    <h6>Total: ${earnings} USD</h6>
                     <h4 className={styles.ogbudget}>
                       (Original budget: ${totalBudget} USD)
                     </h4>
