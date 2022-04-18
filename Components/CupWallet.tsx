08e11bd8be4df264553390f28122ce1b4b6725f0
Roho000 <rohan.samanta@hotmail.com>
2022-04-18 19:42:28 -0400

    updated cupwallet

--- Components/CupWallet.tsx
+++ Components/CupWallet.tsx
@@ -2,21 +2,64 @@ import React, { useContext, useEffect } from "react";
 import { UserContext } from "../context/UserProvider";
 import { useRouter } from "next/router";
 import styles from "../styles/CupDetails.module.css";
+import { makeStyles } from "@material-ui/core/styles";
 import Button from "@mui/material/Button";
 import { Icon } from "@iconify/react";
 import { useState } from "react";
 import { CryptoContext } from "../context/CryptoProvider";
+import Divider from '@mui/material/Divider';
+import { getDoc, Timestamp, doc, onSnapshot } from "firebase/firestore";
+import { db } from "../config/firebase.config";
+import TradeCryptoDialog from "./TradeCryptoDialog";
 type ContentProps = {
   cupid: string;
   portfolios: any;
 };
 const CupWallet = ({ cupid, portfolios }: ContentProps) => {
+  const useStyles = makeStyles((theme) => ({
+    line: {
+      "& ": {
+        backgroundColor: "white",
+        borderWidth: "1px",
+        marginTop: "11px",
+      },
+    },
+  }));
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const classes = useStyles();
+  const router = useRouter();
   const user = useContext(UserContext);
   const cryptos = useContext(CryptoContext);
   const [zScores, setZScores] = useState<any>([{}]);
   const [loading, setLoading] = useState<boolean>(true);
+  const [earnings, setEarnings] = useState(0);
+  const [totalBudget, setTotalBudget] = useState(0);
+  const [cupState, setCupState] = useState("");
+  const [dashboard] = useState(router.asPath === "/dashboard");
+  const {
+    query: { id },
+  } = router;
 
   useEffect(() => {
+
+    const cupDocRef = doc(db, "cups", cupid);
+    onSnapshot(cupDocRef, async (snapshot) => {
+      if (snapshot.exists()) {
+        const data: any = snapshot.data();
+        setCupState(data.currentState);
+        setTotalBudget(data.totalBudget);
+        setEarnings(data.totalBudget);
+        if (user.uid in data.userPortfolios) {
+          let total = 0;
+          Object.entries(data.userPortfolios[user.uid]).map((x: any) => {
+            total = total + cryptos[x[0]].price * x[1];
+          });
+          total = parseFloat(total.toFixed(2));
+          setEarnings(total);
+        }
+      }
+    });
+
     //ranking cryptos based on day % change
     let total = 0;
     let count = 0;
@@ -43,16 +86,13 @@ const CupWallet = ({ cupid, portfolios }: ContentProps) => {
     for (var i in scores) {
       scores[i].rank = i;
     }
-    console.log(scores);
+    //console.log(scores);
 
     setZScores(scores);
     // end ranking algorithm
   }, []);
 
-  const router = useRouter();
-  const {
-    query: { id },
-  } = router;
+  
   const handleUpdate = async () => {
     try {
       const response = await fetch("/api/cryptoinfo", {
@@ -70,17 +110,14 @@ const CupWallet = ({ cupid, portfolios }: ContentProps) => {
   };
   return (
     <div>
+      <div className={`${dashboard ? styles.scroll : ""}`}>
       {Object.keys(portfolios[user.uid])
         .sort()
         .map((key, index) => (
           <div key={key}>
             <div className={styles.walleticon}>
               <Icon
-                icon={
-                  key !== "SHIB"
-                    ? `cryptocurrency:${key.toLowerCase()}`
-                    : "cryptocurrency:sand"
-                }
+                icon={key !== "SHIB" ? `cryptocurrency:${key.toLowerCase()}` : "cryptocurrency:sand"}
                 color="#40bd67"
                 width="28"
                 height="28"
@@ -94,6 +131,7 @@ const CupWallet = ({ cupid, portfolios }: ContentProps) => {
             </h4>
           </div>
         ))}
+      </div>
       <Button
         style={{
           background: "#2F3869",
@@ -112,6 +150,23 @@ const CupWallet = ({ cupid, portfolios }: ContentProps) => {
       >
         Update Crypto Prices
       </Button>
+      <Divider className={classes.line}/>
+      <div className={styles.total}>
+        <h6>Total: ${earnings} USD</h6>
+        <h4 className={styles.ogbudget}>
+          (Original budget: ${totalBudget} USD)
+        </h4>
+      </div>
+      {cupState === "active" && (
+        <div className={styles.center}>
+          <TradeCryptoDialog
+            cup={{
+              id: cupid,
+              userPortfolio: portfolios[user.uid],
+            }}
+          />
+        </div>
+      )}
     </div>
   );
 };
